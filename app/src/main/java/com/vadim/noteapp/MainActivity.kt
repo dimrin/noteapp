7224560546b454fbe57324bf375a46e44cc829d3
Vadim Dobrin <zuka537122@gmail.com>
2022-09-28 23:10:26 +0300

    Finished(add functionality to update for a long press)

--- app/src/main/java/com/vadim/noteapp/MainActivity.kt
+++ app/src/main/java/com/vadim/noteapp/MainActivity.kt
@@ -3,15 +3,21 @@ package com.vadim.noteapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.vadim.noteapp.screen.NoteScreen
+import com.vadim.noteapp.screen.NoteViewModel
 import com.vadim.noteapp.ui.theme.NoteAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +28,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val noteViewModel = viewModel<NoteViewModel>()
+                    NotesApp(noteViewModel)
+
                 }
             }
         }
@@ -30,14 +38,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NotesApp(noteViewModel: NoteViewModel) {
+    val notesList = noteViewModel.noteList.collectAsState().value
+
+    NoteScreen(notes = notesList,
+        onRemoveNote = { noteViewModel.removeNote(it) },
+        onAddNote = { noteViewModel.addNote(it) })
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     NoteAppTheme {
-        Greeting("Android")
+        NoteScreen(notes = emptyList(), onAddNote = {}, onRemoveNote = {})
     }
 }
